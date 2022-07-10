864484810dda48c1d135253c077783f5868a67ae
zinnnn37 <zinnnn37@gmail.com>
2022-07-10 17:34:30 +0900

    libft modified

--- libft/libft.h
+++ libft/libft.h
@@ -16,10 +16,17 @@
 # include <unistd.h>
 # include <stdlib.h>
 
+char	*ft_itoa(int n);
+char	**ft_split(char const *s, char c);
 char	*ft_strchr(const char *s, int c);
 char  *ft_strdup(const char *s);
+char	*ft_striteri(char *s, void (*f)(unsigned int, char *));
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
 char	*ft_strrchr(const char *s, int c);
+char	*ft_strtrim(char cons *s1, char const *set);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
 
 int		ft_atoi(const char *str);
 int		ft_isalnum(int c);
@@ -44,5 +51,9 @@ void	*ft_memchr(const void *s, int c, size_t n);
 void	*ft_memcpy(void *restrict dst, const void *restrict src, size_t n);
 void	*memmove(void *dst, const void *src, size_t len);
 void	*ft_memset(void *b, int c, size_t len);
+void	ft_putchar_fd(char c, int fd);
+void	ft_putendl_fd(char *s, int fd);
+void	ft_putnbr_fd(int n, int fd);
+void	ft_putstr_fd(char *s, int fd);
 
 #endif
