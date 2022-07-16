29982f65e096a3204adec930dbc905259ea30790
zinnnn37 <zinnnn37@gmail.com>
2022-07-16 17:50:33 +0900

    substr modified

--- libft/ft_substr.c
+++ libft/ft_substr.c
@@ -16,16 +16,17 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*res;
 	size_t	i;
-	size_t	s_len;
+	size_t	j;
 
 	if (!s)
 		return (0);
+	if (ft_strlen(s) < start)
+		return (ft_strdup(""));
 	res = (char *)malloc(sizeof(char) * (len + 1));
 	if (!res)
 		return (0);
 	i = 0;
-	s_len = ft_strlen(s);
-	while (i < len && i + start < s_len)
+	while (s[start + i] && i < len)
 	{
 		res[i] = s[start + i];
 		i++;
