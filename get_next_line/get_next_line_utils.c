a2aa94c7afeddc445d2068d790952005da68f48d
zinnnn37 <zinnnn37@gmail.com>
2022-09-17 16:38:23 +0900

    [gnl] strlen

--- get_next_line/get_next_line_utils.c
+++ get_next_line/get_next_line_utils.c
@@ -6,72 +6,25 @@
 /*   By: minjinki <minjinki@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/15 18:23:38 by minjinki          #+#    #+#             */
-/*   Updated: 2022/08/15 18:23:40 by minjinki         ###   ########.fr       */
+/*   Updated: 2022/09/17 16:17:48 by minjinki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-size_t	ft_strlen(const char *s)
+size_t	strlen(char *s)
 {
-	int	cnt;
+	int	len;
 
-	cnt = 0;
-	while (s[cnt])
-		cnt++;
-	return (cnt);
-}
-
-char	*ft_strjoin(char const *s1, char const *s2)
-{
-	int		len1;
-	int		len2;
-	int		i;
-	char	*res;
-
-	if (!s1 || !s2)
+	if (!s)
 		return (NULL);
-	len1 = ft_strlen(s1);
-	len2 = ft_strlen(s2);
-	res = (char *)calloc(len1 + len2 + 1, sizeof(char));
-	if (!res)
-		return (NULL);
-	i = -1;
-	while (++i < len1)
-		res[i] = s1[i];
-	while (++i < len1 + len2 + 1)
-		res[i - 1] = s2[i - 1 - len1];
-	return (res);
+	len = 0;
+	while (s[len])
+		len++;
+	return (len);
 }
 
-char	*ft_strchr(const char *s, int c)
-{
-	while (*(unsigned char *)s != (unsigned char)c)
-	{
-		if (*(unsigned char *)s == '\0')
-			return (NULL);
-		s++;
-	}
-	return ((char *)s);
-}
-
-void	*ft_memset(void *b, int c, size_t len)
-{
-	size_t	i;
-
-	i = 0;
-	while (i < len)
-		((unsigned char *)b)[i++] = (unsigned char)c;
-	return (b);
-}
-
-void	*ft_calloc(size_t count, size_t size)
-{
-	void	*res;
-
-	res = (void *)malloc(count * size);
-	if (!res)
-		return (NULL);
-	ft_memset(res, 0, count * size);
-	return (res);
-}
+// strdup
+// strjoin
+// strchr
+// lstdel
