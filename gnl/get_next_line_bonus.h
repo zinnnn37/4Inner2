c1a64c13cc5c1a6e62ed28c4dbd7389d3a468423
zinnnn37 <zinnnn37@gmail.com>
2022-09-18 16:45:06 +0900

    [gnl] ref

---
+++ gnl/get_next_line_bonus.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: minjinki <minjinki@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/28 15:36:43 by minjinki          #+#    #+#             */
+/*   Updated: 2022/09/18 16:41:59 by minjinki         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# include <unistd.h>
+# include <stdlib.h>
+
+typedef struct s_list
+{
+	int				fd;
+	char			*content;
+	struct s_list	*next;
+}	t_list;
+
+char	*get_next_line(int fd);
+size_t	ft_strlen(const char *s);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strndup(const char *s1, size_t n);
+char	*ft_strjoin(const char *s1, const char *s2);
+char	*ft_lstdel(t_list **head_ptr, t_list *curr_ptr);
+
+#endif
---
+++ gnl/get_next_line_utils.c
@@ -0,0 +1,100 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: minjinki <minjinki@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/28 15:36:43 by minjinki          #+#    #+#             */
+/*   Updated: 2022/09/18 16:42:13 by minjinki         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strchr(const char *str, int c)
+{
+	size_t	i;
+
+	i = ft_strlen(str);
+	if (!c)
+		return (((char *)str + i));
+	while (*str)
+	{
+		if (*str == (unsigned char)c)
+			return ((char *)str);
+		str++;
+	}
+	return (NULL);
+}
+
+char	*ft_strndup(const char *s1, size_t len)
+{
+	size_t	i;
+	char	*result;
+
+	i = 0;
+	result = (char *)malloc((len + 1) * sizeof(char));
+	if (!result)
+		return (NULL);
+	while (i < len)
+	{
+		result[i] = s1[i];
+		i++;
+	}
+	result[i] = '\0';
+	return (result);
+}
+
+char	*ft_strjoin(const char *s1, const char *s2)
+{
+	char	*result;
+	char	*temp;
+
+	temp = (char *)malloc((ft_strlen(s1) + ft_strlen(s2) + 1) * sizeof(char));
+	if (!temp)
+		return (NULL);
+	result = temp;
+	while (*s1)
+		*(temp++) = *(s1++);
+	while (*s2)
+		*(temp++) = *(s2++);
+	*temp = '\0';
+	return (result);
+}
+
+char	*ft_lstdel(t_list **head_ptr, t_list *curr_ptr)
+{
+	t_list	*temp;
+
+	if (*head_ptr == curr_ptr)
+	{
+		*head_ptr = curr_ptr->next;
+		free(curr_ptr->content);
+		curr_ptr->content = NULL;
+		free(curr_ptr);
+		curr_ptr = NULL;
+	}
+	else
+	{
+		temp = *head_ptr;
+		while (temp->next != curr_ptr)
+			temp = temp->next;
+		temp->next = curr_ptr->next;
+		free(curr_ptr->content);
+		curr_ptr->content = NULL;
+		free(curr_ptr);
+		curr_ptr = NULL;
+	}
+	return (NULL);
+}
