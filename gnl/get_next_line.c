5f382b48e54d07ac26044d0d430dd6e1bcaa7d0a
zinnnn37 <zinnnn37@gmail.com>
2022-09-19 14:38:28 +0900

    [gnl] my head gonna bloom tq..

--- gnl/get_next_line.c
+++ gnl/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: minjinki <minjinki@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/28 15:36:43 by minjinki          #+#    #+#             */
-/*   Updated: 2022/09/19 13:55:36 by minjinki         ###   ########.fr       */
+/*   Updated: 2022/09/19 14:30:38 by minjinki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,15 +36,62 @@ t_list	*get_fd(t_list **head, int fd)
 	return (new);
 }
 
+int read_file(t_list *cur, char *buf)
+{
+	char	*tmp;
+	char	*next;
+	int		byte;
+
+	buf = (char *)malloc(sizeof(char) + (BUFFER_SIZE + 1));
+	if (!buf)
+		return (0);
+	while (1)
+	{
+		next = ft_strchr(cur->content, '\n');
+		if (next) // EOF but remain some sentences > should not exit
+			return (1);
+		byte = read(cur->fd, buf, BUFFER_SIZE);
+		if (byte < 0) // EOF > exit
+			return (0);
+		buf[byte] = '\0';
+		tmp = ft_strjoin(cur->content, buf);
+		if (!tmp)
+			return (0);
+		free(cur->content);
+		cur->content = tmp;
+	}
+}
+
+char	*one_line(t_list *cur)
+{
+	char	*res;	
+	int		i;
+
+	if (!(cur->content))
+		return (NULL);
+	i = 0;
+	while (cur->content[i] && cur->content[i] != '\n')
+		i++;
+}
+
 char	*get_next_line(int fd)
 {
 	static t_list	*head;
 	t_list			*cur;
+    char            *buf;
 	char			*line;
+    int             status;
 
 	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, 0, 0) < 0)
 		return (NULL);
 	cur = get_fd(&head, fd);
 	if (!cur)
+		return (NULL);
+    status = read_file(cur, buf);
+	free(buf);
+	if (status == 0)
+		return (NULL);
+	line = one_line(cur);
+	if (!line)
 		return (NULL);
 }
