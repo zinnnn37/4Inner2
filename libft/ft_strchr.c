70f82e275ac315c863a8188c4f609245d2548058
zinnnn37 <zinnnn37@gmail.com>
2022-09-19 14:57:51 +0900

    [libft] ft_strchr null-guard

--- libft/ft_strchr.c
+++ libft/ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: minjinki <minjinki@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/08 13:48:54 by minjinki          #+#    #+#             */
-/*   Updated: 2022/07/20 15:26:59 by minjinki         ###   ########.fr       */
+/*   Updated: 2022/09/19 14:50:14 by minjinki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 
 char	*ft_strchr(const char *s, int c)
 {
+	if (!s)
+		return (NULL);
 	while (*(unsigned char *)s != (unsigned char)c)
 	{
 		if (*(unsigned char *)s == '\0')
