3ed6050ff067a7ab5066c620eb81c2b0661e6649
zinnnn37 <zinnnn37@gmail.com>
2022-09-21 13:31:01 +0900

    [gnl] draft

--- 3rdgnl/get_next_line.c
+++ 3rdgnl/get_next_line.c
@@ -6,33 +6,50 @@
 /*   By: minjinki <minjinki@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/21 10:19:12 by minjinki          #+#    #+#             */
-/*   Updated: 2022/09/21 13:12:37 by minjinki         ###   ########.fr       */
+/*   Updated: 2022/09/21 13:30:40 by minjinki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char	*save_next()
+char	*save_next(t_list **head, t_list *cur, char *next)
 {
+	char	*tmp;
+	char	*line;
+
+	line = ft_strndup(cur->buf, next - (cur->buf) + 1); // +1 은 \n 때문에
+	if (!line || !*line)
+		return (NULL);
+	if (next)
+		tmp = ft_strndup(next + 1, ft_strlen(next + 1));
+	else
+		tmp = ft_strndup("", 0);
+	if (!tmp)
+		return (NULL);
+	free(cur->buf);
+	cur->buf = tmp;
+	if (ft_strlen(cur->buf) <= 0)
+		ft_lst_del_node(&head, cur);
+	return (line);
 }
 
-char	*get_line(t_list *cur, char	*next)
+char	*get_line(t_list **head, t_list *cur, char	*next)
 {
 	char	*line;
 
 	if (next)
-		line = save_next();
+		line = save_next(head, cur, next);
 	else
 	{
 		if (!*(cur->buf))
 			return (NULL);
 		else
-			line = save_next();
+			line = save_next(head, cur, next);
 	}
 	return (line);
 }
 
-char	*read_file(t_list *cur, char *buf)
+char	*read_file(t_list **head, t_list *cur, char *buf)
 {
 	char	*next;
 	char	*tmp;
@@ -54,7 +71,7 @@ char	*read_file(t_list *cur, char *buf)
 		free(cur->buf);
 		cur->buf = tmp;
 	}
-	return (get_line(cur, next));
+	return (get_line(head, cur, next));
 }
 
 t_list	*get_fd(t_list **head, int fd)
@@ -98,11 +115,13 @@ char	*get_next_line(int fd)
 	buf = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
 	if (!buf)
 		return (ft_lst_del_node(&head, cur));
-	line = read_file(cur, buf);
+	line = read_file(head, cur, buf);
+	if (!line)
+		return (ft_lst_del_node(&head, cur));
 	free(buf); // 줄 안 넘을 것 같으면 buf 할당해제 read_file() 안으로 넣기
-
+	return (line);
 }
-
+// null일 때 전부 lst del 해야하네....... 그래야 leak이 안나네......
 /*
 int main(void)
 {
