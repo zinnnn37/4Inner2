d325df0d57dfce643ce984ea36bfb3c482c9d82b
zinnnn37 <zinnnn37@gmail.com>
2022-11-09 16:15:26 +0900

    [union] 1st

--- exam02/union/union.c
+++ exam02/union/union.c
@@ -3,6 +3,25 @@
 int main(int argc, char **argv)
 {
 	int visited[256] = { 0, };
+	int i = 1, j;
 
+	if (argc == 3)
+	{
+		j = 0;
+		while (i < 3)
+		{
+			j = 0;
+			while (argv[i][j])
+			{
+				if (visited[(unsigned char)argv[i][j]] == 0)
+				{
+					visited[(unsigned char)argv[i][j]] = 1;
+					write(1, &argv[i][j], 1);
+				}
+				j++;
+			}
+			i++;
+		}
+	}
 	return 0;
 }
