2e4b74b62138a7a36e92a1e280110cf3deaa0287
zinnnn37 <zinnnn37@gmail.com>
2022-11-15 15:11:21 +0900

    [so_long] add ft_strcmp in libft.h

--- so_long/include/libft.h
+++ so_long/include/libft.h
@@ -6,7 +6,7 @@
 /*   By: minjinki <minjinki@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/07 15:25:24 by minjinki          #+#    #+#             */
-/*   Updated: 2022/09/21 12:29:22 by minjinki         ###   ########.fr       */
+/*   Updated: 2022/11/15 14:56:34 by minjinki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,6 +43,7 @@ int		ft_isprint(int c);
 int		ft_isalnum(int c);
 int		ft_lstsize(t_list *lst);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
+int		ft_strcmp(const char *s1, const char *s2);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 int		ft_tolower(int c);
 int		ft_toupper(int c);
