4fc514de7f00645f4b3ca81a2ff4937305c7d953
zinnnn37 <zinnnn37@gmail.com>
2022-11-16 16:01:32 +0900

    [so_long] check_map draft

--- so_long/src/check_map.c
+++ so_long/src/check_map.c
@@ -6,12 +6,38 @@
 /*   By: minjinki <minjinki@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/16 14:56:57 by minjinki          #+#    #+#             */
-/*   Updated: 2022/11/16 14:57:23 by minjinki         ###   ########.fr       */
+/*   Updated: 2022/11/16 15:38:20 by minjinki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/so_long.h"
 
+void	check_elements(char *line, t_map *map)
+{
+	int	len;
+	int	i;
+}
+
+void	check_valid(int status, char *line, t_map *map)
+{
+	map->height++;
+	if (status == 0)
+		check_wall(line, map);
+	else
+		check_elements(line, map);
+}
+
 void	check_map(t_map *map)
 {
+	int	i;
+
+	check_valid(0, map->map[0], map);
+	i = 0;
+	while (map[i])
+	{
+		check_valid(1, map);
+	}
+	if (map->height < 3 && map->width < 3)
+		print_error("*ERROR* Invalid Map!\n");
+	check_valid(0, map->map[height - 1], map)
 }
