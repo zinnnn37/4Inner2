2c92bb2b4373a452716d54139d43ea5bf224aaac
zinnnn37 <zinnnn37@gmail.com>
2022-11-17 14:07:28 +0900

    [so_long] get_map to init.c

--- so_long/include/so_long.h
+++ so_long/include/so_long.h
@@ -6,7 +6,7 @@
 /*   By: minjinki <minjinki@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/27 11:07:37 by minjinki          #+#    #+#             */
-/*   Updated: 2022/11/17 11:52:45 by minjinki         ###   ########.fr       */
+/*   Updated: 2022/11/17 14:06:08 by minjinki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,7 +41,6 @@ t_map	*ft_init(int argc, char **argv);
 void	check_map(t_map *map);
 void	find_path(t_map *map);
 void	free_all(t_map *map);
-void	get_map(int fd, t_map *map);
 void	print_error(char *str);
 
 #endif
--- so_long/src/init.c
+++ so_long/src/init.c
@@ -6,11 +6,37 @@
 /*   By: minjinki <minjinki@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/16 13:58:52 by minjinki          #+#    #+#             */
-/*   Updated: 2022/11/16 14:26:49 by minjinki         ###   ########.fr       */
+/*   Updated: 2022/11/17 14:06:03 by minjinki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/so_long.h"
+
+void	get_map(int fd, t_map *map)
+{
+	char	*s;
+	char	*line;
+	char	*tmp;
+
+	line = NULL;
+	s = get_next_line(fd);
+	while (s)
+	{
+		if (ft_strlen(s) == 0)
+			break ;
+		tmp = ft_strjoin(line, s);
+		if (!tmp)
+			print_error("*ERROR* Fail to save map!\n");
+		free(line);
+		line = tmp;
+		free(s);
+		s = get_next_line(fd);
+	}
+	map->map = ft_split(line, '\n');
+	if (!map->map)
+		print_error("*ERROR* Fail to save map!\n");
+	free(line);
+}
 
 t_map	*ft_init(int argc, char **argv)
 {
