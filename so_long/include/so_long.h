a9a5aa44a26694b728c12ba1ecc84c731948ade0
zinnnn37 <zinnnn37@gmail.com>
2022-11-29 15:57:40 +0900

    [so_long] key hook start

--- so_long/include/so_long.h
+++ so_long/include/so_long.h
@@ -6,7 +6,7 @@
 /*   By: minjinki <minjinki@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/27 11:07:37 by minjinki          #+#    #+#             */
-/*   Updated: 2022/11/29 10:26:27 by minjinki         ###   ########.fr       */
+/*   Updated: 2022/11/29 12:46:04 by minjinki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@
 # include <mlx.h>
 # include "./libft.h"
 # include "./queue.h"
+/* queue 없애고 stdio.h 여기 넣기 */
 # include <fcntl.h>
 # include <string.h>
 
@@ -23,6 +24,16 @@
 # define FAILURE 1
 # define TRUE 1
 # define FALSE 0
+
+# define W 13
+# define A 0
+# define S 1
+# define D 2
+# define ESC 53
+# define UP -1
+# define DOWN 1
+# define LEFT -1
+# define RIGHT 1
 
 typedef int	t_bool;
 
@@ -35,7 +46,26 @@ typedef struct s_map
 	int		p_y;
 	int		collections;
 	int		exit;
+	t_img	*img;
 }	t_map;
+
+typedef struct s_img
+{
+	void	*back;
+	void	*p_left;
+	void	*p_right;
+	void	*door_closed;
+	void	*door_opend;
+	void	*key;
+}	t_img;
+
+typedef struct s_win
+{
+	int		height;
+	int		width;
+	t_img	*img;
+	t_map	*map;
+}	t_win;
 
 t_map	*ft_init(int argc, char **argv);
 
