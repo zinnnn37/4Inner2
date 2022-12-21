8d236cb7d1e517116d3d084cc25bfe09b74508bc
zinnnn37 <zinnnn37@gmail.com>
2022-12-21 14:59:23 +0900

    [slref] add filename & join == '\0' to NULL

--- slref/libft/ft_strjoin.c
+++ slref/libft/ft_strjoin.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strjoin.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tblaase <tblaase@student.42.fr>            +#+  +:+       +#+        */
+/*   By: minjinki <minjinki@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/06 15:29:26 by tblaase           #+#    #+#             */
-/*   Updated: 2021/10/07 19:00:13 by tblaase          ###   ########.fr       */
+/*   Updated: 2022/12/21 14:26:44 by minjinki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ char	*ft_strjoin(char *s1, const char *s2)
 	i = 0;
 	i2 = 0;
 	c = malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
-	if (c == '\0')
+	if (!c)
 		return (0);
 	while (s1 && s1[i] != '\0')
 	{
