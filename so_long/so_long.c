eb80db67cabdf8abf64a3255d23287e36946a8d8
zinnnn37 <zinnnn37@gmail.com>
2023-01-06 17:35:43 +0900

    [so_long] delete for in main func

--- so_long/so_long.c
+++ so_long/so_long.c
@@ -6,7 +6,7 @@
 /*   By: minjinki <minjinki@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/11 12:28:17 by minjinki          #+#    #+#             */
-/*   Updated: 2023/01/05 19:26:23 by minjinki         ###   ########.fr       */
+/*   Updated: 2023/01/06 17:34:56 by minjinki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,12 +23,6 @@ int	main(int argc, char **argv)
 	if (!data)
 		print_error("Fail to allocate memory!\n");
 	map->data = data;
-	for (int i = 0; i < map->height; i++)
-	{
-		for (int j = 0; i < map->width; j++)
-			printf("%c", map->map[i][j]);
-		printf("\n");
-	}
 	check_map(map);
 	find_path(map);
 	init_game(map);
