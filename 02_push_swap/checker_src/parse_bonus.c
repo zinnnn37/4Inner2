ee3b905bdf4c46a70d478c3e65740e039ba58fc6
zinnnn37 <zinnnn37@gmail.com>
2023-02-02 10:28:02 +0900

    [push_swap] make bonus modified

--- 02_push_swap/checker_src/parse_bonus.c
+++ 02_push_swap/checker_src/parse_bonus.c
@@ -1,20 +1,36 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   checker.c                                          :+:      :+:    :+:   */
+/*   parse.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: minjinki <minjinki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/01/30 12:01:32 by minjinki          #+#    #+#             */
-/*   Updated: 2023/02/02 10:25:23 by minjinki         ###   ########.fr       */
+/*   Created: 2023/01/31 10:57:06 by minjinki          #+#    #+#             */
+/*   Updated: 2023/01/31 13:21:24 by minjinki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../checker_include/checker.h"
+#include "../include/push_swap.h"
 
-int	main(int ac, char **av)
+void	get_numbers(int ac, char **av, t_stack *s)
 {
-	(void)ac;
-	(void)av;
-	write(1, "bonus\n", 6);
+	int		i;
+	int		j;
+	int		data;
+	char	**res;
+
+	i = 0;
+	while (++i < ac)
+	{
+		res = ft_split(av[i]);
+		if (!res)
+			print_error();
+		j = -1;
+		while (res[++j])
+		{
+			if (ft_atoi(res[j], &data) == FALSE)
+				print_error();
+			create_and_add_node(s, data);
+		}
+	}
 }
---
+++ 02_push_swap/checker_src/stack_bonus.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   stack.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: minjinki <minjinki@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/30 12:23:13 by minjinki          #+#    #+#             */
+/*   Updated: 2023/02/02 10:22:36 by minjinki         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/push_swap.h"
+#include <stdio.h>
+
+void	print_nodes(t_stack *s)
+{
+	t_data	*tmp;
+
+	tmp = s->top;
+	printf("\n\n\nsize: %d\n\n", s->size);
+	while (tmp)
+	{
+		printf("%d\n", tmp->data);
+		tmp = tmp->next;
+	}
+}
+
+void	create_and_add_node(t_stack *s, int data)
+{
+	t_data	*node;
+
+	node = (t_data *)ft_calloc(sizeof(t_data), 1);
+	if (!node)
+		print_error();
+	node->prev = NULL;
+	node->data = data;
+	node->next = NULL;
+	if (s->size == 0)
+	{
+		s->top = node;
+		s->bottom = node;
+	}
+	else
+	{
+		s->bottom->next = node;
+		node->prev = s->bottom;
+		s->bottom = node;
+	}
+	s->size++;
+	//print_nodes(s);
+}
