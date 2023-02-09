878c80d25fcaae74143d01c2718627d855c3f333
zinnnn37 <zinnnn37@gmail.com>
2023-02-09 23:14:58 +0900

    [ps] sort modifying

--- 02_push_swap/src/sort.c
+++ 02_push_swap/src/sort.c
@@ -1,75 +1,41 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   sort.c                                             :+:      :+:    :+:   */
+/*   sort_home.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: minjinki <minjinki@student.42.fr>          +#+  +:+       +#+        */
+/*   By: MJKim <zinnnn37@gmail.com>                 +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/02/02 10:28:29 by minjinki          #+#    #+#             */
-/*   Updated: 2023/02/09 11:38:50 by minjinki         ###   ########.fr       */
+/*   Created: 2023/02/09 23:04:26 by MJKim             #+#    #+#             */
+/*   Updated: 2023/02/09 23:04:26 by MJKim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/push_swap.h"
 
-void	a_to_b(t_info *info, int min, int max, int mid) // optimize
+void	a_to_b(t_info *info, int i)
 {
-	int	cnt;
+	int	len;
 
-	cnt = 0;
-	if (info->a->size <= 5)
+	len = info->a->size - 1;
+	while (info->a->size != 0)
 	{
-		sort_small_numbers(info->a, info->b);
-		return ;
-	}
-	while (cnt < info->len / info->chunk)
-	{
-		if (min <= info->a->top->rank && info->a->top->rank <= max)
+		if (info->a->top->rank <= i)
 		{
-			cnt++;
-			pn(info->a, info->b, "pb");
-			if (max - min > 1 && info->b->top->rank > mid)
-				rn(info->b, "rb");
+			pn(a, b, "pb");
+			i++;
 		}
-		rn(info->a, "ra");
-	}
-	min = max;
-	max = max + info->len / info->chunk;
-	mid = (min + max) / 2;
-	if (min < info->len)
-		a_to_b(info, min, max, mid);
-}
-
-void	b_to_a(t_info *info)
-{
-	int	top;
-	int	bottom;
-
-	top = max_from_top(info->b);
-	bottom = max_from_bottom(info->b) + 1;
-	//print_nodes(info->a, info->b);
-	if (top <= bottom)
-	{
-		while (top-- > 0)
+		else if (info->a->top->rank > i && info->a->top->rank <= i + info->chunk)
+		{
+			pn(a, b, "pb");
 			rn(info->b, "rb");
-		pn(info->b, info->a, "pa");
+			i++;
+		}
+		else if (info->a->top->rank > i + info->chunk)
+		{
+			if (i < info->a->size / 2 && i >= 0) // ?
+				rrn(info->a, "rra");
+			else
+				rn(info->a, "ra");
+		}
 	}
-	else
-	{
-		while (bottom-- > 0)
-			rrn(info->b, "rrb");
-		pn(info->b, info->a, "pa");
-	}
-}
-
-void	sort(t_info *info)
-{
-	int	max;
-	int	mid;
-
-	max = info->len / info->chunk;
-	mid = max / 2;
-	a_to_b(info, 0, max, mid);
-	while (info->b->top)
-		b_to_a(info);
 }
