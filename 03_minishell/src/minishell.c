d93b60c663baf16967f87740ea36c35c99dee4e8
zinnnn37 <zinnnn37@gmail.com>
2023-02-20 14:30:56 +0900

    [minishell] cannot find rl_on_new_line

--- 03_minishell/src/minishell.c
+++ 03_minishell/src/minishell.c
@@ -6,7 +6,7 @@
 /*   By: minjinki <minjinki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/16 15:01:52 by minjinki          #+#    #+#             */
-/*   Updated: 2023/02/20 11:44:52 by minjinki         ###   ########.fr       */
+/*   Updated: 2023/02/20 14:12:14 by minjinki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,21 @@ int	main(int ac, char **av)
 	char	*cmd;
 
 	(void)av;
-	if (ac == 1)
+	if (ac != 1)
+		return (FAILURE);
+	while (TRUE)
 	{
-		while (TRUE)
-		{
-			cmd = readline("minishell_$> ");
-			parse(cmd);
-			free(cmd);
-		}
+		cmd = readline("minishell_$ ");
+		if (!cmd)
+			break ;
+		//parse(cmd);
+		printf("%s\n", cmd);
+		add_history(cmd);
+		write(1, "a", 1);
+		rl_on_new_line();
+		rl_replace_line();
+		rl_redisplay();
+		free(cmd);
 	}
 	return (SUCCESS);
 }
--- 03_minishell/src/parse.c
+++ 03_minishell/src/parse.c
@@ -1,27 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   minishell.h                                        :+:      :+:    :+:   */
+/*   parse.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: minjinki <minjinki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/02/16 15:02:08 by minjinki          #+#    #+#             */
-/*   Updated: 2023/02/20 14:14:07 by minjinki         ###   ########.fr       */
+/*   Created: 2023/02/20 13:13:51 by minjinki          #+#    #+#             */
+/*   Updated: 2023/02/20 13:14:22 by minjinki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef MINISHELL_H
-# define MINISHELL_H
+#include "../include/minishell.h"
 
-#include <stdio.h>
-# include <readline/readline.h>
-# include <readline/history.h>
-# include <unistd.h>
-# include <stdlib.h>
-
-# define SUCCESS 0
-# define FAILURE 1
-# define TRUE 1
-# define FALSE 0
-
-#endif
+void	parse(char *cmd)
+{	
+}
