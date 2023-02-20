2ef8de80fa4c759334c264db0ff917ec299fab5e
zinnnn37 <zinnnn37@gmail.com>
2023-02-20 12:58:02 +0900

    [minishell] readline install, test

--- 03_minishell/include/minishell.h
+++ 03_minishell/include/minishell.h
@@ -6,11 +6,21 @@
 /*   By: minjinki <minjinki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/16 15:02:08 by minjinki          #+#    #+#             */
-/*   Updated: 2023/02/16 15:02:24 by minjinki         ###   ########.fr       */
+/*   Updated: 2023/02/20 11:19:45 by minjinki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
+
+# include <readline/readline.h>
+# include <unistd.h>
+# include <stdlib.h>
+#include <stdio.h>
+
+# define SUCCESS 0
+# define FAILURE 1
+# define TRUE 1
+# define FALSE 0
 
 #endif
--- 03_minishell/src/minishell.c
+++ 03_minishell/src/minishell.c
@@ -6,8 +6,25 @@
 /*   By: minjinki <minjinki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/16 15:01:52 by minjinki          #+#    #+#             */
-/*   Updated: 2023/02/16 15:02:03 by minjinki         ###   ########.fr       */
+/*   Updated: 2023/02/20 11:44:52 by minjinki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
+
+int	main(int ac, char **av)
+{
+	char	*cmd;
+
+	(void)av;
+	if (ac == 1)
+	{
+		while (TRUE)
+		{
+			cmd = readline("minishell_$> ");
+			parse(cmd);
+			free(cmd);
+		}
+	}
+	return (SUCCESS);
+}
