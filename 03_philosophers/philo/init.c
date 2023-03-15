00eee8fae548d7de9d02b595f35df002609fd5f6
zinnnn37 <zinnnn37@gmail.com>
2023-03-15 09:39:29 +0900

    [philo] init nums to nums * 1000: to match with get_cur_time()

--- 03_philosophers/philo/init.c
+++ 03_philosophers/philo/init.c
@@ -14,17 +14,22 @@
 
 t_bool	parsing_nums(int ac, char **av, t_data *data)
 {
+	int	n;
+
 	if (!ft_atoi(av[1], &data->nums) || data->nums < 1)
 		return (print_error("Check if the arguments are valid: av[1]"));
-	if (!ft_atoi(av[2], &data->ttdie) || data->ttdie < 1)
+	if (!ft_atoi(av[2], &n) || n < 1)
 		return (print_error("Check if the arguments are valid: av[2]"));
-	if (!ft_atoi(av[3], &data->tteat) || data->tteat < 1)
+	data->ttdie = n * 1000;
+	if (!ft_atoi(av[3], &n) || n < 1)
 		return (print_error("Check if the arguments are valid: av[3]"));
-	if (!ft_atoi(av[4], &data->ttsleep) || data->ttsleep < 1)
+	data->tteat = n * 1000;
+	if (!ft_atoi(av[4], &n) || n < 1)
 		return (print_error("Check if the arguments are valid: av[4]"));
-	if (ac == 6 && (!ft_atoi(av[5], &data->limit) || data->limit < 1))
+	data->ttsleep = n * 1000;
+	if (ac == 6 && (!ft_atoi(av[5], &data->limit) || data->limit < 0))
 		return (print_error("Check if the arguments are valid: av[5]"));
-	else if (ac == 5)
+	else if (ac == 5 || av[5] == 0)
 		data->limit = -1;
 	return (TRUE);
 }
