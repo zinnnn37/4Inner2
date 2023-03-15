bcaeb949beb2405af3b049ae23fbe51aca44203f
zinnnn37 <zinnnn37@gmail.com>
2023-03-15 14:20:47 +0900

    [philo] remove comments in main.c

--- 03_philosophers/philo/main.c
+++ 03_philosophers/philo/main.c
@@ -29,30 +29,3 @@ int	main(int ac, char **av)
 	//free_all();
 	return (SUCCESS);
 }
-
-/*
-	1. 구조체 초기화
-	2. av 5개인지 6개인지 확인하고 limiter 0 혹은 av[5]로 초기화
-	3. 
-*/
-
-
-/*
-	홀수 짝수로 나눠서
-	홀수는 왼쪽 포크부터, 짝수는 오른쪽 포크부터 집게 해서 데드락 방지
-	
-*/
-
-
-/*
-int	main(void)
-{
-	struct timeval	start;
-	struct timeval	end;
-
-	gettimeofday(&start, NULL);
-	sleep(1);
-	gettimeofday(&end, NULL);
-	printf("%ld, %d\n", end.tv_sec - start.tv_sec, end.tv_usec - start.tv_usec);
-}
-*/
