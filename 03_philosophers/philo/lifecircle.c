ee58d621a1a8b8c648a5d6a624cf524d17b990ab
zinnnn37 <zinnnn37@gmail.com>
2023-03-15 12:54:05 +0900

    [philo] eating

--- 03_philosophers/philo/lifecircle.c
+++ 03_philosophers/philo/lifecircle.c
@@ -25,6 +25,18 @@ void	lifecircle(t_philo *philo)
 	}
 }
 
+t_bool	is_one_philo(t_philo *philo)
+{
+	if (philo->data->nums == 1)
+	{
+		put_fork(philo->rfork);
+		while (!(philo->is_dead))
+			usleep(10);
+		return (TRUE);
+	}
+	return (FALSE);
+}
+
 void	sleeping(t_philo *philo)
 {
 	display_msg(philo, SLEEP_MSG);
@@ -34,7 +46,31 @@ void	sleeping(t_philo *philo)
 
 void	eating(t_philo *philo)
 {
-
+	if (philo->id % 2 == 1)
+	{
+		pick_fork(philo->lfork);
+		display_msg(philo, FORK_MSG);
+		pick_fork(philo->rfork);
+		display_msg(philo, FORK_MSG);
+	}
+	else
+	{
+		pick_fork(philo->rfork);
+		display_msg(philo, FORK_MSG);
+		if (is_one_philo(philo))
+			return ;
+		pick_fork(philo->lfork);
+		display_msg(philo, FORK_MSG);
+	}
+	display_msg(philo, EAT_MSG);
+	do_usleep(philo->data->time_to_eat);
+	philo->eat_cnt++;
+	philo->state = SLEEPING;
+	put_fork(philo->lfork);
+	put_fork(philo->rfork);
+	lock_philo(philo);
+	philo->last_eat = get_cur_time();
+	unlock_philo(philo);
 }
 
 void	thinking(t_philo *philo)
--- 03_philosophers/philo/print.c
+++ 03_philosophers/philo/print.c
@@ -16,7 +16,7 @@ void	display_msg(t_philo *philo, char *msg)
 {
 	lock_philo(philo);
 	if (!(philo->is_dead))
-		printf("%zu %u %s\n", get_cur_time() / 1000, philo->id, msg);
+		printf("%zu %u %s\n", get_cur_time() / 1000, philo->id + 1, msg);
 	unlock_philo(philo);
 }
 // philo가 아니라 mprint로 mutex 바꿀지 말지 실행해보고 결정
