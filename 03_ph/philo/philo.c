57a97fcc9cd3471668ed3afac002169f8cce04b0
zinnnn37 <zinnnn37@gmail.com>
2023-04-12 13:00:21 +0900

    [philo] routine

--- 03_ph/philo/philo.c
+++ 03_ph/philo/philo.c
@@ -12,11 +12,51 @@
 
 #include "philo.h"
 
+void	eating_n_sleeping(t_philo *philo)
+{
+	pthread_mutex_lock(&(philo->table->forks[philo->fork[0]]));
+	msg(philo, "has taken a fork");
+	pthread_mutex_lock(&(philo->table->forks[philo->fork[1]]));
+	msg(philo, "has taken a fork");
+	msg(philo, "is eating");
+	pthread_mutex_lock(&(philo->meal));
+	philo->last_eat = get_cur_time();
+	pthread_mutex_unlock(&(philo->meal));
+	do_usleep(philo->data, philo->data->tteat);
+	if (!is_simul_stopped(philo->data))
+	{
+		pthread_mutex_lock(&(philo->meal));
+		philo->num_eat++;
+		pthread_mutex_unlock(&(philo->meal));
+	}
+	msg(philo, "is sleeping");
+	pthread_mutex_unlock(&(philo->table->forks[philo->fork[1]]));
+	pthread_mutex_unlock(&(philo->table->forks[philo->fork[0]]));
+	do_usleep(philo->data, philo->data->ttsleep)
+}
+
+void	thinking(t_philo *philo, int print)
+{
+	time_t	ttthink;
+
+	pthread_mutex_lock(&(philo->meal));
+	ttthink = philo->data->ttdie - (get_cur_time() - philo->last_eat) \
+		- philo->data->tteat;
+	pthread_mutex_unlock(&(philo->meal));
+	if (ttthink < 0)
+		ttthink = 0;
+	if (ttthink > 500)
+		ttthink = 500;
+	if (print)
+		msg(philo, "is thinking");
+	do_usleep(philo->data, ttthink);
+}
+
 void	*one_philo(t_philo *philo)
 {
-	pthread_mutex_lock(&(philo->table->forks[philo->fork[0]]));\
+	pthread_mutex_lock(&(philo->table->forks[philo->fork[0]]));
 	msg(philo, "has taken a fork");
-	do_usleep(philo->data, philo->data->tteat);
+	do_usleep(philo->data, philo->data->ttdie);
 	msg(philo, "is dead");
 	pthread_mutex_unlock(&(philo->table->forks[philo->fork[0]]));
 }
@@ -37,11 +77,11 @@ void	*philosopher(void *arg)
 	if (philo->data->num_philos == 1)
 		return (one_philo(philo));
 	else if (philo->id % 2)
-		thinking(philo, TRUE);
+		thinking(philo, FALSE);
 	while (!is_simul_stopped(philo->data))
 	{
 		eating_n_sleeping(philo);
-		thinking(philo, FALSE);
+		thinking(philo, TRUE);
 	}
 	return (NULL);
 }
