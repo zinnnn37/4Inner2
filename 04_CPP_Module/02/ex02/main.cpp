361507b012abfe1fc205b6b188bf18d7b68a3854
zinnnn37 <zinnnn37@gmail.com>
2023-06-26 19:15:51 +0900

    [CPP02] ex02: done

--- 04_CPP_Module/02/ex02/main.cpp
+++ 04_CPP_Module/02/ex02/main.cpp
@@ -6,25 +6,26 @@
 /*   By: minjinki <minjinki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/23 18:50:46 by minjinki          #+#    #+#             */
-/*   Updated: 2023/06/26 18:26:47 by minjinki         ###   ########.fr       */
+/*   Updated: 2023/06/26 19:15:04 by minjinki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
 
-int main( void ) {
-  Fixed a;
-  Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
+int main( void )
+{
+	Fixed a;
+	Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
 
-  std::cout << a << std::endl;
-  std::cout << ++a << std::endl;
-  std::cout << a << std::endl;
-  std::cout << a++ << std::endl;
-  std::cout << a << std::endl;
+	std::cout << a << std::endl;
+	std::cout << ++a << std::endl;
+	std::cout << a << std::endl;
+	std::cout << a++ << std::endl;
+	std::cout << a << std::endl;
 
-  std::cout << b << std::endl;
+	std::cout << b << std::endl;
 
-  std::cout << Fixed::max( a, b ) << std::endl;
+	std::cout << Fixed::max( a, b ) << std::endl;
 
-  return 0;
+	return 0;
 }
