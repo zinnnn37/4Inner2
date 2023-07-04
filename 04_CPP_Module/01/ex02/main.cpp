9ad358a5f6e5165da08773e4c99f41b1ebb3ed10
zinnnn37 <zinnnn37@gmail.com>
2023-07-04 14:13:08 +0900

    [CPP01] ex02: change name of vlas

--- 04_CPP_Module/01/ex02/main.cpp
+++ 04_CPP_Module/01/ex02/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: minjinki <minjinki@student.42.fr>          +#+  +:+       +#+        */
+/*   By: minjinki <minjinki@student.42.kr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/26 13:21:39 by minjinki          #+#    #+#             */
-/*   Updated: 2023/06/26 11:56:36 by minjinki         ###   ########.fr       */
+/*   Updated: 2023/07/04 14:12:39 by minjinki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,22 +15,19 @@
 
 int	main( void )
 {
-	std::string	str = "HI THIS IS BRAIN";
-	std::string	*strPTR = &str;
-	std::string& strREF = str;
+	std::string	string = "HI THIS IS BRAIN";
+	std::string	*stringPTR = &string;
+	std::string& stringREF = string;
 
 	std::cout << "Address:" << std::endl;
-	std::cout << "str   : " << &str << std::endl;
-	std::cout << "strPTR: " << &strPTR << std::endl;
-	std::cout << "strREF: " << &strREF << std::endl << std::endl;
+	std::cout << "str   : " << &string << std::endl;
+	std::cout << "strPTR: " << &stringPTR << std::endl;
+	std::cout << "strREF: " << &stringREF << std::endl << std::endl;
 
 	std::cout << "Content:" << std::endl;
-	std::cout << "str   : " << str << std::endl;
-	std::cout << "strPTR: " << strPTR << std::endl;
-	std::cout << "strREF: " << strREF << std::endl;
+	std::cout << "str   : " << string << std::endl;
+	std::cout << "strPTR: " << stringPTR << std::endl;
+	std::cout << "strREF: " << stringREF << std::endl;
 	
 	return (0);
 }
-
-// PTR: str의 주소값을 담고 있는 공간
-// REF: str 그 자체, 단지 이름만 다를 뿐
