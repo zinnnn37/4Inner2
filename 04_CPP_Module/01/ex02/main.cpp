322881ae27afb5680fa22022342022d048528431
zinnnn37 <zinnnn37@gmail.com>
2023-07-05 11:48:11 +0900

    [CPP01] ex02: edit align

--- 04_CPP_Module/01/ex02/main.cpp
+++ 04_CPP_Module/01/ex02/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: minjinki <minjinki@student.42.kr>          +#+  +:+       +#+        */
+/*   By: minjinki <minjinki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/26 13:21:39 by minjinki          #+#    #+#             */
-/*   Updated: 2023/07/04 14:12:39 by minjinki         ###   ########.fr       */
+/*   Updated: 2023/07/05 11:39:24 by minjinki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,14 +20,14 @@ int	main( void )
 	std::string& stringREF = string;
 
 	std::cout << "Address:" << std::endl;
-	std::cout << "str   : " << &string << std::endl;
-	std::cout << "strPTR: " << &stringPTR << std::endl;
-	std::cout << "strREF: " << &stringREF << std::endl << std::endl;
+	std::cout << "string : " << &string << std::endl;
+	std::cout << "strPTR : " << &stringPTR << std::endl;
+	std::cout << "strREF : " << &stringREF << std::endl << std::endl;
 
 	std::cout << "Content:" << std::endl;
-	std::cout << "str   : " << string << std::endl;
-	std::cout << "strPTR: " << stringPTR << std::endl;
-	std::cout << "strREF: " << stringREF << std::endl;
+	std::cout << "string : " << string << std::endl;
+	std::cout << "strPTR : " << stringPTR << std::endl;
+	std::cout << "strREF : " << stringREF << std::endl;
 	
 	return (0);
 }
