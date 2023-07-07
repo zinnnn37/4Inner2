63be6f6b67b4fc4eef5e6c0f1721af6cc197b209
zinnnn37 <zinnnn37@gmail.com>
2023-07-07 12:03:05 +0900

    [CPP04] ex00: main: test case

--- 04_CPP_Module/04/ex00/main.cpp
+++ 04_CPP_Module/04/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: minjinki <minjinki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/29 14:24:19 by minjinki          #+#    #+#             */
-/*   Updated: 2023/06/29 17:27:25 by minjinki         ###   ########.fr       */
+/*   Updated: 2023/07/07 12:02:42 by minjinki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,37 @@
 int main()
 {
 	const Animal* meta = new Animal();
-	const Animal* j = new Dog();
 	const Animal* i = new Cat();
+	const Animal* j = new Dog();
 	
-	std::cout << j->getType() << " " << std::endl;
 	std::cout << i->getType() << " " << std::endl;
+	std::cout << j->getType() << " " << std::endl;
 	i->makeSound(); //will output the cat sound!
 	j->makeSound();
 	meta->makeSound();
+
+	Animal	copy = Animal(*meta);
+	Cat		copycat = Cat(*((Cat*)i));
+	Dog		copydog = Dog(*((Dog*)j));
+
+	std::cout << copy.getType() << " " << std::endl;
+	copy.makeSound();
+	std::cout << copycat.getType() << " " << std::endl;
+	copycat.makeSound();
+	std::cout << copydog.getType() << " " << std::endl;
+	copydog.makeSound();
+
+	delete meta;
+	delete i;
+	delete j;
+	
+	std::cout << std::endl << "Copy test" << std::endl;
+	std::cout << copy.getType() << " " << std::endl;
+	copy.makeSound();
+	std::cout << copycat.getType() << " " << std::endl;
+	copycat.makeSound();
+	std::cout << copydog.getType() << " " << std::endl;
+	copydog.makeSound();
 
 	return 0;
 }
