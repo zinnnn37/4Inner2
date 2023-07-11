bb43d013825680687c6f369c7539439d43b7608d
zinnnn37 <zinnnn37@gmail.com>
2023-07-11 13:41:25 +0900

    [EXAM04]

---
+++ 04_exam04/microshell.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   microshell.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: minjinki <minjinki@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/11 13:22:51 by minjinki          #+#    #+#             */
+/*   Updated: 2023/07/11 13:36:37 by minjinki         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MICROSHELL_H
+# define MICROSHELL_H
+
+# include <string.h>
+# include <unistd.h>
+# include <sys/wait.h>
+# include <stdio.h>
+
+#endif
