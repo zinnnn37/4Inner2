c403f6cd37d979f421a0ebce9b923d34504943e6
zinnnn37 <zinnnn37@gmail.com>
2023-07-11 17:40:40 +0900

    [CPP03] ex02: std::endl to \n

--- 04_CPP_Module/03/ex02/ClapTrap.cpp
+++ 04_CPP_Module/03/ex02/ClapTrap.cpp
@@ -6,7 +6,7 @@
 /*   By: minjinki <minjinki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/26 21:50:59 by minjinki          #+#    #+#             */
-/*   Updated: 2023/07/11 14:09:03 by minjinki         ###   ########.fr       */
+/*   Updated: 2023/07/11 17:36:16 by minjinki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,33 +15,33 @@
 ClapTrap::ClapTrap()
 	: _name("default"), _hit(10), _energy(10), _damage(0)
 {
-	std::cout << CYAN"[ Default Constructor ]" << std::endl;
-	std::cout << WHITE"ClapTrap < default > has been created" << std::endl << std::endl;
-	std::cout << "----------------------------------------------------------" << std::endl << std::endl;
+	std::cout << CYAN"[ Default Constructor ]\n";
+	std::cout << WHITE"ClapTrap < default > has been created\n\n";
+	std::cout << "----------------------------------------------------------\n" << std::endl;
 }
 
 ClapTrap::ClapTrap( const std::string& name )
 	: _name(name), _hit(10), _energy(10), _damage(0)
 {
-	std::cout << CYAN"[ Constructor ]" << std::endl;
-	std::cout << WHITE"ClapTrap < " << this->_name << " > has been created" << std::endl << std::endl;
-	std::cout << "----------------------------------------------------------" << std::endl << std::endl;
+	std::cout << CYAN"[ Constructor ]\n";
+	std::cout << WHITE"ClapTrap < " << this->_name << " > has been created\n\n";
+	std::cout << "----------------------------------------------------------\n" << std::endl;
 }
 
 ClapTrap::ClapTrap( const ClapTrap& ct )
 {
-	std::cout << CYAN"[ Copy Constructor ]" << std::endl;
-	std::cout << WHITE"ClapTrap < " << ct._name << " > has been copied" << std::endl << std::endl;
-	std::cout << "----------------------------------------------------------" << std::endl << std::endl;
+	std::cout << CYAN"[ Copy Constructor ]\n";
+	std::cout << WHITE"ClapTrap < " << ct._name << " > has been copied\n\n";
+	std::cout << "----------------------------------------------------------\n" << std::endl;
 
 	*this = ct;
 }
 
 ClapTrap::~ClapTrap()
 {
-	std::cout << CYAN"[ Destructor ]" << std::endl;
-	std::cout << WHITE"ClapTrap < " << this->_name << " > has gone" << std::endl << std::endl;
-	std::cout << "----------------------------------------------------------" << std::endl << std::endl;
+	std::cout << CYAN"[ Destructor ]\n";
+	std::cout << WHITE"ClapTrap < " << this->_name << " > has gone\n\n";
+	std::cout << "----------------------------------------------------------\n" << std::endl;
 }
 
 ClapTrap&	ClapTrap::operator=( const ClapTrap& ct )
@@ -61,61 +61,61 @@ void	ClapTrap::attack( const std::string& target )
 {
 	std::cout << CYAN"[ Attack ]" << std::endl;
 	std::cout << WHITE"ClapTrap < " << this->_name << " > ";
-	std::cout << "tries to attack < " << target << " >" << std::endl;
+	std::cout << "tries to attack < " << target << " >\n";
 
 	if (this->_energy == 0)
 	{
-		std::cout << RED"▶︎ Out of energy! Fail to attack!" << std::endl << std::endl;
-		std::cout << WHITE"----------------------------------------------------------" << std::endl << std::endl;
+		std::cout << RED"▶︎ Out of energy! Fail to attack!\n\n";
+		std::cout << WHITE"----------------------------------------------------------\n" << std::endl;
 		return ;
 	}
 	else if (this->_hit <= 0)
 	{
-		std::cout << RED"▶︎ Out of hits! Fail to attack!" << std::endl << std::endl;
-		std::cout << WHITE"----------------------------------------------------------" << std::endl << std::endl;
+		std::cout << RED"▶︎ Out of hits! Fail to attack!\n\n";
+		std::cout << WHITE"----------------------------------------------------------\n" << std::endl;
 		return ;
 	}
 
 	std::cout << RED"▶︎ < " << target << " > got < " << this->_damage;
-	std::cout << " > points of damage!" << std::endl << std::endl;
-	std::cout << WHITE"----------------------------------------------------------" << std::endl << std::endl;
+	std::cout << " > points of damage!\n\n";
+	std::cout << WHITE"----------------------------------------------------------\n" << std::endl;
 
 	this->_energy--;
 }
 
 void	ClapTrap::takeDamage( unsigned int amount )
 {
-	std::cout << CYAN"[ Take Damage ]" << std::endl;
+	std::cout << CYAN"[ Take Damage ]\n";
 
 	std::cout << RED"▶︎ ClapTrap < " << this->_name << " > got < ";
-	std::cout << amount << " > points of damage!" << std::endl << std::endl;
-	std::cout << WHITE"----------------------------------------------------------" << std::endl << std::endl;
+	std::cout << amount << " > points of damage!\n\n";
+	std::cout << WHITE"----------------------------------------------------------\n" << std::endl;
 
 	this->_hit -= amount;
 }
 
 void	ClapTrap::beRepaired( unsigned int amount )
 {
-	std::cout << CYAN"[ Reparing ]" << std::endl;
+	std::cout << CYAN"[ Reparing ]\n";
 
-	std::cout << WHITE"ClapTrap < " << this->_name << " > is repairing" << std::endl;
+	std::cout << WHITE"ClapTrap < " << this->_name << " > is repairing\n";
 	
 	if (this->_energy == 0)
 	{
-		std::cout << RED"▶︎ Out of energy! Fail to repair!" << std::endl;
-		std::cout << WHITE"----------------------------------------------------------" << std::endl << std::endl;
+		std::cout << RED"▶︎ Out of energy! Fail to repair!\n\n";
+		std::cout << WHITE"----------------------------------------------------------\n" << std::endl;
 		return ;
 	}
 	else if (this->_hit <= 0)
 	{
-		std::cout << RED"▶︎ Out of hits! Fail to repair!" << std::endl << std::endl;
-		std::cout << WHITE"----------------------------------------------------------" << std::endl << std::endl;
+		std::cout << RED"▶︎ Out of hits! Fail to repair!\n\n";
+		std::cout << WHITE"----------------------------------------------------------\n" << std::endl;
 		return ;
 	}
 
 	std::cout << BLUE"▶︎ ClapTrap < " << this->_name << " > got < ";
-	std::cout << amount << " > points of Hits!" << std::endl << std::endl;
-	std::cout << WHITE"----------------------------------------------------------" << std::endl << std::endl;
+	std::cout << amount << " > points of Hits!\n\n";
+	std::cout << WHITE"----------------------------------------------------------\n" << std::endl;
 
 	this->_hit += amount;
 	this->_energy--;
@@ -123,11 +123,11 @@ void	ClapTrap::beRepaired( unsigned int amount )
 
 void	ClapTrap::print()
 {
-	std::cout << CYAN"========================< STATUS >========================" << std::endl << std::endl;
-	std::cout << "\t\t   NAME\t\t  : " << this->_name << std::endl;
-	std::cout << "\t\t   HIT POINTS     : " << this->_hit << std::endl;
-	std::cout << "\t\t   ENERGY POINTS  : " << this->_energy << std::endl;
-	std::cout << "\t\t   ATTACK DAMAGE  : " << this->_damage << std::endl << std::endl;
-	std::cout << "==========================================================" << std::endl << std::endl;
-	std::cout << WHITE"----------------------------------------------------------" << std::endl << std::endl;
+	std::cout << CYAN"========================< STATUS >========================\n\n";
+	std::cout << "\t\t   NAME\t\t  : " << this->_name << '\n';
+	std::cout << "\t\t   HIT POINTS     : " << this->_hit << '\n';
+	std::cout << "\t\t   ENERGY POINTS  : " << this->_energy << '\n';
+	std::cout << "\t\t   ATTACK DAMAGE  : " << this->_damage << "\n\n";
+	std::cout << "==========================================================\n\n";
+	std::cout << WHITE"----------------------------------------------------------\n" << std::endl;
 }
