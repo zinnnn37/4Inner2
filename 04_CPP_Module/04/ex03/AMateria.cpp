6657fce8f361a006a0449c7d407213befd10ae75
zinnnn37 <zinnnn37@gmail.com>
2023-07-13 23:18:42 +0900

    [CPP04] ex03: AMateria

--- 04_CPP_Module/04/ex03/AMateria.cpp
+++ 04_CPP_Module/04/ex03/AMateria.cpp
@@ -3,37 +3,38 @@
 /*                                                        :::      ::::::::   */
 /*   AMateria.cpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: minjinki <minjinki@student.42.fr>          +#+  +:+       +#+        */
+/*   By: minjinki <minjinki@student.42.kr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/13 18:23:25 by minjinki          #+#    #+#             */
-/*   Updated: 2023/07/13 18:35:28 by minjinki         ###   ########.fr       */
+/*   Updated: 2023/07/13 23:17:24 by minjinki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "AMateria.hpp"
+#include "ICharacter.hpp"
 
 AMateria::AMateria()
 	: _type("default")
 {
-	std::cout << PURPLE"[ Default Constructor ]" << WHITE << std::endl;
+	std::cout << PURPLE"[ Default Constructor ]\n" << WHITE << std::endl;
 }
 
 AMateria::AMateria( std::string const &type )
 	: _type(type)
 {
-	std::cout << PURPLE"[ Constructor: type ]" << WHITE << std::endl;
+	std::cout << PURPLE"[ Constructor: type ]\n" << WHITE << std::endl;
 }
 
 AMateria::AMateria( const AMateria& a )
 {
-	std::cout << PURPLE"[ Copy Constructor ]" << WHITE << std::endl;
+	std::cout << PURPLE"[ Copy Constructor ]\n" << WHITE << std::endl;
 
 	this->_type = a._type;
 }
 
 AMateria::~AMateria()
 {
-	std::cout << PURPLE"[ Destructor ]" << WHITE << std::endl;
+	std::cout << PURPLE"[ Destructor ]\n" << WHITE << std::endl;
 }
 
 AMateria&	AMateria::operator=( const AMateria& a )
@@ -41,7 +42,7 @@ AMateria&	AMateria::operator=( const AMateria& a )
 	std::cout << PURPLE"[ Copy Assignment Operator ]" << WHITE << std::endl;
 
 	if (this != &a)
-		this->_type = a._type;
+		this->_type = a.getType();
 
 	return (*this);
 }
@@ -59,6 +60,5 @@ AMateria*	AMateria::clone() const
 void	AMateria::use( ICharacter& target)
 {
 	std::cout << PURPLE"[ Skill ]" << WHITE << std::endl;
-
-	(void)target;
+	std::cout << "Use Skill of " << target.getName() << "\n" << std::endl;
 }
--- 04_CPP_Module/04/ex03/AMateria.hpp
+++ 04_CPP_Module/04/ex03/AMateria.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   AMateria.hpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: minjinki <minjinki@student.42.fr>          +#+  +:+       +#+        */
+/*   By: minjinki <minjinki@student.42.kr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/13 15:53:44 by minjinki          #+#    #+#             */
-/*   Updated: 2023/07/13 18:57:35 by minjinki         ###   ########.fr       */
+/*   Updated: 2023/07/13 23:13:04 by minjinki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,6 @@
 
 #include <iostream>
 #include <string>
-#include "ICharacter.hpp"
 
 #define WHITE "\033[37m"
 #define PURPLE "\033[35m"
@@ -23,22 +22,22 @@
 #define GREEN "\033[32m"
 #define YELLOW "\033[33m"
 
+class ICharacter;
+
 class AMateria
 {
-	private:
-		AMateria();
-
 	protected:
 		std::string	_type;
 
 	public:
+		AMateria();
 		AMateria( std::string const &type );
 		AMateria( const AMateria& m );
 		virtual ~AMateria();
 
 		AMateria&	operator=( const AMateria& m );
 
-		std::string const &getType() const;
+		std::string const&	getType() const;
 
 		virtual AMateria*	clone() const = 0;
 		virtual void		use( ICharacter& target );
