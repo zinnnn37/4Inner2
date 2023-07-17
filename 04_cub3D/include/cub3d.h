e251a9956084b204eefbf28a88699909a6964b44
zinnnn37 <zinnnn37@gmail.com>
2023-07-17 16:25:33 +0900

    [CUB3D]

---
+++ 04_cub3D/include/cub3d.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub3d.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: minjinki <minjinki@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/17 16:24:17 by minjinki          #+#    #+#             */
+/*   Updated: 2023/07/17 16:25:11 by minjinki         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CUB3D_H
+# define CUB3D_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <math.h>
+# include "../mlx/mlx.h"
+
+#endif
