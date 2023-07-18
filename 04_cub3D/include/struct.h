9a6e9076fb509011926b6db3ceb3e87c087140ce
zinnnn37 <zinnnn37@gmail.com>
2023-07-18 14:22:39 +0900

    [CUB3D] no --save in updated subject

--- 04_cub3D/include/struct.h
+++ 04_cub3D/include/struct.h
@@ -6,7 +6,7 @@
 /*   By: minjinki <minjinki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/18 13:20:39 by minjinki          #+#    #+#             */
-/*   Updated: 2023/07/18 13:25:58 by minjinki         ###   ########.fr       */
+/*   Updated: 2023/07/18 14:22:02 by minjinki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,7 +40,6 @@ typedef struct s_game
 	void	*mlx;
 	void	*win;
 
-	int		bmp;
 	int		width;
 	int		height;
 
--- 04_cub3D/src/MAIN/main.c
+++ 04_cub3D/src/MAIN/main.c
@@ -6,7 +6,7 @@
 /*   By: minjinki <minjinki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/17 16:23:40 by minjinki          #+#    #+#             */
-/*   Updated: 2023/07/18 13:03:53 by minjinki         ###   ########.fr       */
+/*   Updated: 2023/07/18 14:22:15 by minjinki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,8 @@
 
 int	main(int ac, char **av)
 {
-	if (ac == 2 || ac == 3)
-	{
-		if (ft_strcmp(av[2], "--save") != 0)
-			return (print_error("Error\nInvalid argument"));
-	}
-	else
-		return (print_error("Error\nInvalid argument"));
+	t_game	game;
+
+	if (ac != 2)
+		return (print_error("Error\nUsage: ./cub3d file.cub [--save]\n"));
 }
