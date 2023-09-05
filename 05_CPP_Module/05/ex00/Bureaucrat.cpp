2496640ec99300501bcfd2916f9f87871c281853
zinnnn37 <zinnnn37@gmail.com>
2023-09-05 14:00:46 +0900

    [CPP05] should code exception class

--- 05_CPP_Module/05/ex00/Bureaucrat.cpp
+++ 05_CPP_Module/05/ex00/Bureaucrat.cpp
@@ -3,12 +3,52 @@
 /*                                                        :::      ::::::::   */
 /*   Bureaucrat.cpp                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: minjinki <minjinki@student.42.fr>          +#+  +:+       +#+        */
+/*   By: minjinki <minjinki@student.42.kr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/29 12:41:19 by minjinki          #+#    #+#             */
-/*   Updated: 2023/08/29 12:42:17 by minjinki         ###   ########.fr       */
+/*   Updated: 2023/09/05 14:00:27 by minjinki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include Bureaucrat.hpp
+#include "Bureaucrat.hpp"
 
+Bureaucrat::Bureaucrat()
+	: _name("default"), _grade(150)
+{
+}
+
+Bureaucrat::Bureaucrat( std::string name, int grade )
+	: _name(name), _grade(grade)
+{
+	if (_grade < 1)
+		throw GradeTooHighException();
+	else if (_grade > 150)
+		throw GradeTooLowException();
+}
+
+Bureaucrat::Bureaucrat( const Bureaucrat &b )
+{
+	*this = b;
+}
+
+Bureaucrat::~Bureaucrat()
+{
+}
+
+Bureaucrat&	Bureaucrat::operator=( const Bureaucrat &b )
+{
+	if (this != &b)
+		this->_grade = b.getGrade();
+
+	return (*this);
+}
+
+const std::string	Bureaucrat::getName() const
+{
+	return (_name);
+}
+
+int	Bureaucrat::getGrade() const
+{
+	return (_grade);
+}
--- 05_CPP_Module/05/ex00/Bureaucrat.hpp
+++ 05_CPP_Module/05/ex00/Bureaucrat.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Bureaucrat.hpp                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: minjinki <minjinki@student.42.fr>          +#+  +:+       +#+        */
+/*   By: minjinki <minjinki@student.42.kr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/29 12:41:37 by minjinki          #+#    #+#             */
-/*   Updated: 2023/08/29 13:03:35 by minjinki         ###   ########.fr       */
+/*   Updated: 2023/09/05 14:00:03 by minjinki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,23 +18,31 @@
 class Bureaucrat
 {
 	private:
-		const std::string	name;
-		int					grade;
+		const std::string	_name;
+		int					_grade;
 
 	public:
 		Bureaucrat();
 		Bureaucrat( std::string name, int grade );
-		Bureaucrat( const Bureaucrat &bureaucrat );
+		Bureaucrat( const Bureaucrat &b );
 		~Bureaucrat();
+
+		Bureaucrat&			operator=( const Bureaucrat &b );
 
 		const std::string	getName() const;
 		int					getGrade() const;
-};
 
-class GradeTooHighException : public std::exception
-{
-	public:
-		// GradeTooHighException();
+		class GradeTooHighException : public std::exception
+		{
+			public:
+				
+		};
+
+		class GradeTooLowException : public std::exception
+		{
+			public:
+				
+		};
 };
 
 #endif
