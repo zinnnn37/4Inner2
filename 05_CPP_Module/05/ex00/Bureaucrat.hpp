a2be643a5293fcfd32088cd2c47c4214bd39f118
zinnnn37 <zinnnn37@gmail.com>
2023-09-13 10:55:34 +0900

    [CPP05] add exception header

--- 05_CPP_Module/05/ex00/Bureaucrat.hpp
+++ 05_CPP_Module/05/ex00/Bureaucrat.hpp
@@ -6,7 +6,7 @@
 /*   By: minjinki <minjinki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/29 12:41:37 by minjinki          #+#    #+#             */
-/*   Updated: 2023/09/11 11:09:37 by minjinki         ###   ########.fr       */
+/*   Updated: 2023/09/13 10:55:15 by minjinki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 #define BUREAUCRAT_HPP
 
 #include <iostream>
+#include <exception>
 
 class Bureaucrat
 {
