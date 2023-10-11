3d8f9b9ce20782a91b18f53097e6dee01f8c89e9
zinnnn37 <zinnnn37@gmail.com>
2023-10-11 13:30:53 +0900

    [CPP06][ex01] add print code of raw

--- 05_CPP_Module/06/ex01/main.cpp
+++ 05_CPP_Module/06/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: minjinki <minjinki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/11 13:13:03 by minjinki          #+#    #+#             */
-/*   Updated: 2023/10/11 13:23:07 by minjinki         ###   ########.fr       */
+/*   Updated: 2023/10/11 13:29:13 by minjinki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,10 @@ int	main(void)
 	std::cout << "data->str:\t" << data->str << std::endl;
 	std::cout << "data->i:\t" << data->i << std::endl;
 
+	std::cout << "\n------------ serialized data -------------\n" << std::endl;
+	
 	raw = Serializer::serialize(data);
+	std::cout << "raw:\t" << raw << std::endl;
 
 	std::cout << "\n---------- after serialization -----------\n" << std::endl;
 
