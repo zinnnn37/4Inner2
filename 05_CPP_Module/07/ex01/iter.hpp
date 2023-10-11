8c839f12469d98a43d0f4cac7dca42c8999b1fef
zinnnn37 <zinnnn37@gmail.com>
2023-10-11 16:12:30 +0900

    [CPP07][ex01] int len to unsigned int len

--- 05_CPP_Module/07/ex01/iter.hpp
+++ 05_CPP_Module/07/ex01/iter.hpp
@@ -6,7 +6,7 @@
 /*   By: minjinki <minjinki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/11 15:40:11 by minjinki          #+#    #+#             */
-/*   Updated: 2023/10/11 16:02:41 by minjinki         ###   ########.fr       */
+/*   Updated: 2023/10/11 16:12:16 by minjinki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@
 #include <string>
 
 template <typename T>
-void	iter(T *arr, int len, void ( *f )( T & ))
+void	iter(T *arr, unsigned int len, void ( *f )( T & ))
 {
 	for (int i = 0; i < len; i++)
 		f(arr[i]);
