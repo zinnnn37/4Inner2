a83bf5d022234d389c1cc4f8d2f7b414ac56d3e2
zinnnn37 <zinnnn37@gmail.com>
2023-10-11 14:00:40 +0900

    [CPP06][ex02] done

--- 05_CPP_Module/06/ex02/Base.hpp
+++ 05_CPP_Module/06/ex02/Base.hpp
@@ -6,7 +6,7 @@
 /*   By: minjinki <minjinki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/11 13:33:40 by minjinki          #+#    #+#             */
-/*   Updated: 2023/10/11 13:35:57 by minjinki         ###   ########.fr       */
+/*   Updated: 2023/10/11 13:50:10 by minjinki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,23 @@
 #define BASE_HPP
 
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 
 class	Base
 {
 	private:
 
 	public:
-		Base();
-		Base( const Base &b );
 		virtual ~Base();
-
-		Base	&operator=( const Base &b );
-
-		Base	*generate( void );
-		void	identify( Base *p );
-		void	identify( Base &p );
 };
+
+class	A : public Base {};
+class	B : public Base {};
+class	C : public Base {};
+
+Base	*generate( void );
+void	identify( Base *p );
+void	identify( Base &p );
 
 #endif
--- 05_CPP_Module/06/ex02/main.cpp
+++ 05_CPP_Module/06/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: minjinki <minjinki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/11 13:34:30 by minjinki          #+#    #+#             */
-/*   Updated: 2023/10/11 13:34:47 by minjinki         ###   ########.fr       */
+/*   Updated: 2023/10/11 13:58:55 by minjinki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,15 @@
 
 int	main(void)
 {
+	Base	*base;
+
+	srand(time(NULL));
+
+	base = generate();
+	identify(base);
+	identify(*base);
+
+	delete base;
+
 	return (0);
 }
