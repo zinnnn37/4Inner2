d7d9bb5c12737f48231d6c0880470aad0d9ef84b
zinnnn37 <zinnnn37@gmail.com>
2023-10-11 18:44:56 +0900

    [CPP08][ex00]

---
+++ 05_CPP_Module/08/ex00/easyfind.hpp
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   easyfind.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: minjinki <minjinki@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/11 16:27:56 by minjinki          #+#    #+#             */
+/*   Updated: 2023/10/11 16:40:40 by minjinki         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef EASYFIND_HPP
+#define EASYFIND_HPP
+
+#include <iostream>
+#include <string>
+
+template <typename T>
+
+#endif
