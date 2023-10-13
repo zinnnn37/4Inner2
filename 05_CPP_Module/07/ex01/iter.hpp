ea414b305dfcc745d3826f057155ec0673670396
zinnnn37 <zinnnn37@gmail.com>
2023-10-13 10:01:05 +0900

    [CPP07][ex01] need const func

--- 05_CPP_Module/07/ex01/iter.hpp
+++ 05_CPP_Module/07/ex01/iter.hpp
@@ -6,7 +6,7 @@
 /*   By: minjinki <minjinki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/11 15:40:11 by minjinki          #+#    #+#             */
-/*   Updated: 2023/10/11 16:12:16 by minjinki         ###   ########.fr       */
+/*   Updated: 2023/10/13 10:00:45 by minjinki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,5 +22,7 @@ void	iter(T *arr, unsigned int len, void ( *f )( T & ))
 	for (int i = 0; i < len; i++)
 		f(arr[i]);
 }
+
+// const도 만들어야 함
 
 #endif
