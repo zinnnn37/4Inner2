230388a050149b2e7dbd3894fd86ab92b452584b
zinnnn37 <zinnnn37@gmail.com>
2023-10-15 11:15:02 +0900

    [CPP07][ex01] add const

--- 05_CPP_Module/07/ex01/iter.hpp
+++ 05_CPP_Module/07/ex01/iter.hpp
@@ -6,7 +6,7 @@
 /*   By: minjinki <minjinki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/11 15:40:11 by minjinki          #+#    #+#             */
-/*   Updated: 2023/10/13 10:00:45 by minjinki         ###   ########.fr       */
+/*   Updated: 2023/10/15 11:14:37 by minjinki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,16 @@
 #include <string>
 
 template <typename T>
-void	iter(T *arr, unsigned int len, void ( *f )( T & ))
+void	iter( T *arr, unsigned int len, void( *f )( T & ) )
 {
-	for (int i = 0; i < len; i++)
+	for (unsigned int i = 0; i < len; i++)
+		f(arr[i]);
+}
+
+template <typename T>
+void	iter( const T *arr, unsigned int len, void( *f )( const T & ) )
+{
+	for (unsigned int i = 0; i < len; i++)
 		f(arr[i]);
 }
 
