802f3b80b0ae1ba59926065a69a23f8ad30ae2c0
zinnnn37 <zinnnn37@gmail.com>
2023-10-17 16:54:08 +0900

    [CPP08][ex02] start

---
+++ 05_CPP_Module/08/ex02/MutantStack.hpp
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   MutantStack.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: minjinki <minjinki@student.42.kr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/17 16:52:21 by minjinki          #+#    #+#             */
+/*   Updated: 2023/10/17 16:52:55 by minjinki         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MUTANTSTACK_HPP
+#define MUTANTSTACK_HPP
+
+#include <iostream>
+#include <stack>
+#include <deque>
+#include <algorithm>
+#include <exception>
+
+#endif
