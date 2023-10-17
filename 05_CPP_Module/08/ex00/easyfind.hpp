72486cc828170688c24d6e8cd6ec2997b8a713dc
zinnnn37 <zinnnn37@gmail.com>
2023-10-17 13:49:41 +0900

    [CPP08][ex00] done

--- 05_CPP_Module/08/ex00/easyfind.hpp
+++ 05_CPP_Module/08/ex00/easyfind.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   easyfind.hpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: minjinki <minjinki@student.42.fr>          +#+  +:+       +#+        */
+/*   By: minjinki <minjinki@student.42.kr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/11 16:27:56 by minjinki          #+#    #+#             */
-/*   Updated: 2023/10/11 16:40:40 by minjinki         ###   ########.fr       */
+/*   Updated: 2023/10/17 13:43:26 by minjinki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,30 @@
 #define EASYFIND_HPP
 
 #include <iostream>
-#include <string>
+#include <exception>
+#include <algorithm>
+#include <deque>
+
+class	NotFoundException : public std::exception
+{
+	public:
+		virtual const char	*what() const throw()
+		{
+			return ("Occurence Not Found");
+		}
+};
 
 template <typename T>
+int	easyfind( T &t, int n )
+{
+	typename T::iterator it;
+
+	it = std::find(t.begin(), t.end(), n);
+
+	if (it == t.end())
+		throw NotFoundException();
+	
+	return (it - t.begin());
+}
 
 #endif
--- 05_CPP_Module/08/ex00/main.cpp
+++ 05_CPP_Module/08/ex00/main.cpp
@@ -3,10 +3,66 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: minjinki <minjinki@student.42.fr>          +#+  +:+       +#+        */
+/*   By: minjinki <minjinki@student.42.kr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/11 16:27:47 by minjinki          #+#    #+#             */
-/*   Updated: 2023/10/11 16:27:48 by minjinki         ###   ########.fr       */
+/*   Updated: 2023/10/17 13:49:04 by minjinki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "easyfind.hpp"
+#include <deque>
+
+int	main()
+{
+	std::cout << "========== integer test ==========\n" << std::endl;
+
+	std::deque<int>	deque;
+
+	for (int i = 0; i < 5; i++)
+		deque.push_back(i);
+	
+	try
+	{
+		std::cout << "find 3 in deque" << std::endl;
+		int	idx = easyfind(deque, 3);
+		std::cout << "res: " << idx << std::endl;
+		std::cout << "deque[res]: " << deque[idx] << std::endl;
+
+		std::cout << "\nfind 5 in deque" << std::endl;
+		idx = easyfind(deque, 5);
+		std::cout << "res: " << idx << std::endl;
+		std::cout << "deque[res]: " << deque[idx] << std::endl;
+	}
+	catch( const std::exception &e )
+	{
+		std::cerr << e.what() << '\n' << std::endl;
+	}
+
+	std::cout << "=========== char test ============\n" << std::endl;
+
+	std::deque<char>	deque2;
+
+	for (int i = 0; i < 5; i++)
+		deque2.push_back(i + 'a');
+
+	try
+	{
+		std::cout << "find e in deque" << std::endl;
+		int	idx = easyfind(deque2, 'e');
+		std::cout << "res: " << idx << std::endl;
+		std::cout << "deque[res]: " << deque2[idx] << std::endl;
+
+		std::cout << "\nfind v in deque" << std::endl;
+		idx = easyfind(deque2, 'v');
+		std::cout << "res: " << idx << std::endl;
+		std::cout << "deque[res]: " << deque2[idx] << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n' << std::endl;
+	}
+	
+
+	return 0;
+}
