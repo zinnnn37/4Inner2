243ddde6548a4499ce776559bd72d8c0379dafff
zinnnn37 <zinnnn37@gmail.com>
2023-10-22 17:28:23 +0900

    [CPP09][ex00] Error

--- 05_CPP_Module/09/ex00/BitcoinExchange.cpp
+++ 05_CPP_Module/09/ex00/BitcoinExchange.cpp
@@ -6,13 +6,20 @@
 /*   By: minjinki <minjinki@student.42.kr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/22 15:37:04 by minjinki          #+#    #+#             */
-/*   Updated: 2023/10/22 17:17:10 by minjinki         ###   ########.fr       */
+/*   Updated: 2023/10/22 17:28:11 by minjinki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "BitcoinExchange.hpp"
 
 BitcoinExchange::BitcoinExchange() {}
+
+BitcoinExchange::BitcoinExchange( std::string input )
+	: _input(input)
+{
+	this->readFile();
+	this->parsing();
+}
 
 BitcoinExchange::BitcoinExchange( const BitcoinExchange &be )
 {
@@ -32,4 +39,26 @@ BitcoinExchange	&BitcoinExchange::operator=( const BitcoinExchange &be )
 			this->_data[it->first] = it->second;
 	}
 	return (*this);
+}
+
+
+
+BitcoinExchange::Error::Error() {}
+
+BitcoinExchange::Error::Error( const Error &e )
+{
+	*this = e;
+}
+
+BitcoinExchange::Error::~Error() throw() {}
+
+BitcoinExchange::Error	&BitcoinExchange::Error::operator=( const Error &e )
+{
+	(void)e;
+	return (*this);
+}
+
+const char	*BitcoinExchange::Error::what() const throw()
+{
+	return ("Error");
 }
--- 05_CPP_Module/09/ex00/BitcoinExchange.hpp
+++ 05_CPP_Module/09/ex00/BitcoinExchange.hpp
@@ -6,7 +6,7 @@
 /*   By: minjinki <minjinki@student.42.kr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/22 15:37:18 by minjinki          #+#    #+#             */
-/*   Updated: 2023/10/22 17:17:05 by minjinki         ###   ########.fr       */
+/*   Updated: 2023/10/22 17:27:40 by minjinki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,18 +22,34 @@
 class	BitcoinExchange
 {
 	private:
-		float							_val;
-		std::string						_date;
+		std::string						_input;
 		std::map<std::string, float>	_data;
 
-		BitcoinExchange();
+
+		void	readFile();
+		void	parsing();
 
 	public:
+		BitcoinExchange();
+		BitcoinExchange( std::string input );
 		BitcoinExchange( const BitcoinExchange &be );
 		~BitcoinExchange();
 
 		BitcoinExchange	&operator=( const BitcoinExchange &be );
 
+		void	execute();
+
+	class	Error : public std::exception
+	{
+		public:
+			Error();
+			Error( const Error &e );
+			~Error() throw();
+
+			Error	&operator=( const Error &e );
+
+			const char	*what() const throw();
+	};
 };
 
 #endif
