2b82407b40dd803f0639b60881f64d182981bd59
zinnnn37 <zinnnn37@gmail.com>
2023-10-22 15:21:00 +0900

    [CPP08][ex02] done

--- 05_CPP_Module/08/ex02/MutantStack.hpp
+++ 05_CPP_Module/08/ex02/MutantStack.hpp
@@ -6,7 +6,7 @@
 /*   By: minjinki <minjinki@student.42.kr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/17 16:52:21 by minjinki          #+#    #+#             */
-/*   Updated: 2023/10/19 12:03:16 by minjinki         ###   ########.fr       */
+/*   Updated: 2023/10/22 15:17:29 by minjinki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,11 +26,38 @@
 template <typename T>
 class	MutantStack : public std::stack<T>
 {
-	private:
-
-
 	public:
+		MutantStack() {}
 
+		MutantStack( const MutantStack &ms )
+		{
+			*this = ms;
+		}
+
+		~MutantStack() {}
+
+		MutantStack &operator=( const MutantStack &ms )
+		{
+			if (this != &ms)
+				*this = ms;
+			return (*this);
+		}
+
+		typedef typename std::stack<T>::container_type::iterator iterator;
+		iterator begin() {return this->c.begin();}	// stack::c의 자료형은 deque이며 protected
+		iterator end() {return this->c.end();}
+
+		typedef typename std::stack<T>::container_type::reverse_iterator reverse_iterator;
+		reverse_iterator rbegin() {return this->c.rbegin();}
+		reverse_iterator rend() {return this->c.rend();}
+
+		typedef typename std::stack<T>::container_type::const_iterator const_iterator;
+		const_iterator begin() const {return this->c.begin();}
+		const_iterator end() const {return this->c.end();}
+
+		typedef typename std::stack<T>::container_type::const_reverse_iterator const_reverse_iterator;
+		const_reverse_iterator rbegin() const {return this->c.rbegin();}
+		const_reverse_iterator rend() const {return this->c.rend();}
 };
 
 #endif
--- 05_CPP_Module/08/ex02/main.cpp
+++ 05_CPP_Module/08/ex02/main.cpp
@@ -6,13 +6,79 @@
 /*   By: minjinki <minjinki@student.42.kr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/17 16:53:46 by minjinki          #+#    #+#             */
-/*   Updated: 2023/10/17 16:53:56 by minjinki         ###   ########.fr       */
+/*   Updated: 2023/10/22 15:20:21 by minjinki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "MutantStack.hpp"
+#include <list>
 
 int	main()
 {
+	std::cout << "========== Mutant Stack ==========\n";
+	{
+		MutantStack<int> mstack;
+
+		mstack.push(5);
+		mstack.push(17);
+
+		std::cout << mstack.top() << std::endl;
+
+		mstack.pop();
+
+		std::cout << mstack.size() << std::endl;
+
+		mstack.push(3);
+		mstack.push(5);
+		mstack.push(737);
+		mstack.push(0);
+
+		MutantStack<int>::iterator it = mstack.begin();
+		MutantStack<int>::iterator ite = mstack.end();
+
+		++it;
+		--it;
+
+		while (it != ite)
+		{
+			std::cout << *it << std::endl;
+			++it;
+		}
+		std::stack<int> s(mstack);
+	}
+
+	std::cout << "\n=============== list ===============\n";
+
+	{
+		std::list<int> list;
+
+		list.push_back(5);
+		list.push_back(17);
+
+		std::cout << list.back() << std::endl;
+
+		list.pop_back();
+
+		std::cout << list.size() << std::endl;
+
+		list.push_back(3);
+		list.push_back(5);
+		list.push_back(737);
+		list.push_back(0);
+
+		std::list<int>::iterator it = list.begin();
+		std::list<int>::iterator ite = list.end();
+
+		++it;
+		--it;
+
+		while (it != ite)
+		{
+			std::cout << *it << std::endl;
+			++it;
+		}
+		std::list<int> s(list);
+	}
+
 	return 0;
 }
