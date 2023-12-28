c1e296aaf7186dbb36e60f52d8f78223afc7c379
zinnnn37 <zinnnn37@gmail.com>
2023-12-28 17:38:29 +0900

    [CPP09] 100%

--- 05_CPP_Module/09/ex02/script.sh
+++ 05_CPP_Module/09/ex02/script.sh
@@ -1,2 +1,3 @@
-./PmergeMe `jot -r 30 1 100000 | tr '\n' ' '`
+# ./PmergeMe `jot -r 3000 1 3000 | tr '\n' ' '`
 # ./PmergeMe `shuf -i 1-10 -n 3000 | tr "\n" " "`
+ARGS="$(seq 500 | sort -R | xargs)" && ./PmergeMe $ARGS
